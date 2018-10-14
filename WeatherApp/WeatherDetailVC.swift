300b172a218c8dcf1d9d6ec67aa5b79255f5bc8e
Bhavik Kathiriya <bhoomi@bhoomis-macbook-air.local>
2018-10-14 13:52:17 +0530

    Added static icon images in collection view

--- WeatherApp/WeatherDetailVC.swift
+++ WeatherApp/WeatherDetailVC.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CollectionViewCell : UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblValue: UILabel!
+
+
 }
 
 class WeatherDetailVC: UIViewController {
@@ -18,7 +22,7 @@ class WeatherDetailVC: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    
+    var arrTitleImage : NSMutableArray? = NSMutableArray()
     var objLocation : Location?
     
     private let locaionPresenter = LocationPresenter(locationService: LocationService())
@@ -31,6 +35,34 @@ class WeatherDetailVC: UIViewController {
 
     func commonInit()
     {
+        let dictTemp : NSMutableDictionary = NSMutableDictionary()
+        dictTemp.setValue("Temperature", forKey: "title")
+        dictTemp.setValue("temp.png", forKey: "image")
+
+        self.arrTitleImage?.add(dictTemp)
+        
+        let dictHumidity : NSMutableDictionary = NSMutableDictionary()
+        dictHumidity.setValue("Humidity", forKey: "title")
+        dictHumidity.setValue("humidity", forKey: "image")
+
+        self.arrTitleImage?.add(dictHumidity)
+        
+        
+        let dictRainC : NSMutableDictionary = NSMutableDictionary()
+        dictRainC.setValue("Rain", forKey: "title")
+        dictRainC.setValue("rain", forKey: "image")
+
+        self.arrTitleImage?.add(dictRainC)
+        
+        let dictWind : NSMutableDictionary = NSMutableDictionary()
+        dictWind.setValue("Wind", forKey: "title")
+        dictWind.setValue("wind", forKey: "image")
+
+        self.arrTitleImage?.add(dictWind)
+        
+        self.collectionView.reloadData()
+
+        
         locaionPresenter.getLocationData(objLocation: self.objLocation!)
         
     }
@@ -68,13 +100,16 @@ extension WeatherDetailVC : UICollectionViewDataSource
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return (self.arrTitleImage?.count)!
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
         
-        cell.imageView.backgroundColor = UIColor.gray
+        let dictTitle : NSMutableDictionary = self.arrTitleImage?.object(at: indexPath.row) as! NSMutableDictionary
+        cell.lblTitle.text = dictTitle.value(forKey: "title") as? String
+        let image : UIImage = UIImage(named:dictTitle.value(forKey: "image") as! String)!
+        cell.imageView.image = image
         
         return cell
     }
---
+++ WeatherApp/WeatherInfo.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherInfo.swift
+//  WeatherApp
+//
+//  Created by Bhoomi Kathiriya on 14/10/18.
+//  Copyright © 2018 Bhoomi Kathiriya. All rights reserved.
+//
+
+import Foundation
